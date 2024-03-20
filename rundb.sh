d7624da40f32d8531810bc598f032c1489c4731c
TsungMin-Chang <sandychang37@gmail.com>
2024-03-20 23:59:40 +0800

    run db + deploy

---
+++ rundb.sh
@@ -0,0 +1,10 @@
+podman volume create pg-data
+podman run -d -it --replace \
+  --name chendlar \
+  -e POSTGRES_DB=chendlar \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=postgres \
+  -e PGDATA=/var/lib/postgresql/data \
+  -v pg-data:/var/lib/postgresql/data \
+  -p 127.0.0.1:4999:5432 \
+  postgres:16
